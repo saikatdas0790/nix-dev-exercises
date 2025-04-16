fad351cca5008f71090f43e1b35a64a17cf875eb
Saikat Das <saikatdas0790@gmail.com>
2025-04-16 16:06:40 +0000

    Add .gitignore and initial Nix expressions for hello package

---
+++ hello.nix
@@ -0,0 +1,10 @@
+{ stdenv, fetchzip }:
+stdenv.mkDerivation {
+  name = "hello";
+  version = "2.12.1";
+
+  src = fetchzip {
+    url = "https://ftp.gnu.org/gnu/hello/hello-2.12.1.tar.gz";
+    sha256 = "sha256-1kJjhtlsAkpNB7f6tZEs+dbKd8z7KoNHyDHEJ0tmhnc=";
+  };
+}
